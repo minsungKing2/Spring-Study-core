f0b36798b0fa51459133f8fcf74f1114efda7f64
MinSung Jung <minpro22@naver.com>
2024-12-03 16:13:55 +0900

    24.12.03 first commit

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,17 +12,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor //생성자 자동으로 만들어줌. lombok
+//@RequiredArgsConstructor //생성자 자동으로 만들어줌. lombok
 public class OrderServiceImpl implements OrderService{
     //    private final MemberRepository memberRepository = new MemoryMemberRepository(); //멤버 받아오기
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //할인율 고정 할인
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //할인율 차등 할인(변경 용이 but, OCP 위반! 및 DIP 위반)
 
-    //    @Autowired //의존관계를 주입해줌. 단, 생성자가 한개일 경우 생략 가능.
-    /*public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    @Autowired //의존관계를 주입해줌. 단, 생성자가 한개일 경우 생략 가능.
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }*/
+    }
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; //DIP 위반X 위해 변경
@@ -35,8 +35,8 @@ public class OrderServiceImpl implements OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice); //최종 주문 반환
     }
 
-//    //테스트 용도
-//    public MemberRepository getMemberRepository() {
-//        return memberRepository;
-//    }
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
