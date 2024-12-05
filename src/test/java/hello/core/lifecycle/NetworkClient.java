dfce98b5ff28c39e592996e504927f605c36219d
MinSung Jung <minpro22@naver.com>
2024-12-05 17:15:13 +0900

    24.12.05 second commit

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package hello.core.lifecycle;
 
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +28,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @PostConstruct //의존관계 주입이 끝나면, 호출해주는 메서드
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy //빈이 종료될 때, 호출됨.
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
